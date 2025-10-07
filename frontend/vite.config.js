163d89fbff2265e2f27bf7cf8264502c88911ab8
khatch2 <20730681+khatch2@users.noreply.github.com>
2025-10-07 21:34:07 +0200

    chore: add vitest configuration to vite.config.js for improved testing setup

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,21 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig, loadEnv } from "vite";
 
+// Add this import:
+import { configDefaults } from "vitest/config";
+
 export default defineConfig(({ mode }) => {
-	const env = loadEnv(mode, process.cwd(), "");
-	const isNetlify = !!env.NETLIFY;
-	return {
-		plugins: [react()],
-		base: isNetlify ? "/" : "/drinkrecept/",
-	};
+  const env = loadEnv(mode, process.cwd(), "");
+  const isNetlify = !!env.NETLIFY;
+  return {
+    plugins: [react()],
+    base: isNetlify ? "/" : "/drinkrecept/",
+    // Add this vitest config:
+    test: {
+      environment: "jsdom",
+      // Optionally, you can add globals: true if you want to use global test functions without importing them
+      // globals: true,
+      // ...configDefaults.test,
+    },
+  };
 });
