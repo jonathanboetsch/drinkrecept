71eab72916344e65a81c6f6be89588e4ad1ad480
khatch2 <20730681+khatch2@users.noreply.github.com>
2025-10-07 23:03:52 +0200

    refactor: convert jest.config.js to ES module syntax

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,9 +1,7 @@
-module.exports = {
-  testEnvironment: 'jsdom',
-  transform: {
-    '^.+\\.jsx?$': 'babel-jest',
-  },
-  moduleFileExtensions: ['js', 'jsx', 'json', 'node'],
-  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
-  setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
+export const testEnvironment = "jsdom";
+export const transform = {
+  "^.+\\.jsx?$": "babel-jest",
 };
+export const moduleFileExtensions = ["js", "jsx", "json", "node"];
+export const testPathIgnorePatterns = ["/node_modules/", "/dist/"];
+export const setupFilesAfterEnv = ["<rootDir>/src/setupTests.js"];
