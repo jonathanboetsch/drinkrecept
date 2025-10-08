60f163325e5bcb05a47822aa8e54a4abc0b659ae
khatch2 <20730681+khatch2@users.noreply.github.com>
2025-10-08 11:32:02 +0200

    test: remove Recipe component test file and jest configuration

--- frontend/jest.config.js
+++
@@ -1,7 +0,0 @@
-export const testEnvironment = "jsdom";
-export const transform = {
-  "^.+\\.jsx?$": "babel-jest",
-};
-export const moduleFileExtensions = ["js", "jsx", "json", "node"];
-export const testPathIgnorePatterns = ["/node_modules/", "/dist/"];
-export const setupFilesAfterEnv = ["<rootDir>/src/setupTests.js"];
