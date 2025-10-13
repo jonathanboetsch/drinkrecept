32fc883a458e2b5d0db80732aa28c191d5e36fed
Jonathan Boetsch <jonathan.boetsch@gmail.com>
2025-10-13 15:04:49 +0200

    feat(route): add basename and dynamically set depending on env variables

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,10 +4,15 @@ import "./index.css";
 import App from "./components/App.jsx";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 
+const ghPagesFlag = import.meta.env.VITE_GH_PAGE;
+const prNumber = import.meta.env.VITE_PR_NUMBER;
+
+const basename = ghPagesFlag ? `/drinkrecept/pr-${prNumber}` : "/";
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <Routes>
+      <Routes basename={basename}>
         <Route path="/" element={<App />} />
       </Routes>
     </BrowserRouter>
