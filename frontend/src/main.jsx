d1118f1e06b63c25ac4ff2d408d707ed3c42e37e
khatch2 <20730681+khatch2@users.noreply.github.com>
2025-10-13 14:56:52 +0200

    refactor(main): simplify routing by removing unnecessary Routes and Route components

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,9 +8,7 @@ import { Routes, Route, BrowserRouter } from "react-router-dom";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-      </Routes>
+      <App />
     </BrowserRouter>
   </StrictMode>
 );
