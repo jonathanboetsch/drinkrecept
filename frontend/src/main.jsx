d9b9568668ed1fd87fc4aad88426d81cf254fd8b
Jonathan Boetsch <jonathan.boetsch@gmail.com>
2025-10-13 15:10:42 +0200

    fix(deploy): rename variable import and position basename after BrowserRouter instead of Routes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,15 +4,15 @@ import "./index.css";
 import App from "./components/App.jsx";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 
-const ghPagesFlag = import.meta.env.VITE_GH_PAGE;
+const ghPagesFlag = import.meta.env.VITE_GH_PAGES;
 const prNumber = import.meta.env.VITE_PR_NUMBER;
 
-const basename = ghPagesFlag ? `/drinkrecept/pr-${prNumber}` : "/";
+const basename = ghPagesFlag ? `/drinkrecept/pr-${prNumber}/` : "/";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes basename={basename}>
+    <BrowserRouter basename={basename}>
+      <Routes>
         <Route path="/" element={<App />} />
       </Routes>
     </BrowserRouter>
