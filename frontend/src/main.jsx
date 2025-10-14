f3e53310785ee7b18616b46dd30878531a80581e
khatch2 <20730681+khatch2@users.noreply.github.com>
2025-10-14 12:54:22 +0200

    refactor(main): remove unused Routes and Route imports from main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,8 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./components/App.jsx";
-import { Routes, Route, BrowserRouter } from "react-router-dom";
-
+import {BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
