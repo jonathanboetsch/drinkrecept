9612721cc3a8136775d47f37c56ba8ccf307d8a5
Jonathan Boetsch <jonathan.boetsch@gmail.com>
2025-10-21 13:46:26 +0200

    feat(rating): add state var and setter to Star
    Allows dynamic setting of CSS property hidden for all the Star components simultaneously

--- frontend/src/components/RatingForm.jsx
+++ frontend/src/components/RatingForm.jsx
@@ -1,10 +1,11 @@
 import Star from "./Star.jsx";
 import "./Rating.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function RatingForm({ ratingLevels = [1, 2, 3, 4, 5], confirmationAction, recipe }) {
   const [rating, setRating] = useState(null);
   // TODO: add a mechanism to store and update all the recipes ratings.
+  const [isHidden, setHiding] = useState(false);
 
   return (
     <div className="rating-form">
@@ -13,8 +14,10 @@ export default function RatingForm({ ratingLevels = [1, 2, 3, 4, 5], confirmatio
           key={i}
           level={val} // each Star gets an increasing rating value
           confAction={confirmationAction} // thought to popup a "thank for your contribution"
-          ratingSetter={setRating} // user click on Star => set its rating value in the RatingForm
+          setRating={setRating} // user click on Star => set its rating value in the RatingForm
           recipe={recipe} // optional, put here for logging in the console when user clicks
+          isHidden={isHidden} // Star can this way update its css property dynamically
+          setHiding={setHiding} // A click on a Star sets hiding for all the Star components (isHidden=true)
         />
       ))}
     </div>
