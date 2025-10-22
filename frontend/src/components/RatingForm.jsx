c6dd70a993294a33093dced8b17ac989cea9304f
Jonathan Boetsch <jonathan.boetsch@gmail.com>
2025-10-22 13:56:07 +0200

    refactor(RatingForm): replace useEffect() by a function to handle post and fetch after user click for rating the recipe

--- frontend/src/components/RatingForm.jsx
+++ frontend/src/components/RatingForm.jsx
@@ -1,14 +1,13 @@
 import Star from "./Star.jsx";
 import "./Rating.css";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 export default function RatingForm({ ratingLevels = [1, 2, 3, 4, 5], confirmationAction, recipe }) {
-  const [rating, setRating] = useState(null);
   // TODO: add a mechanism to store and update all the recipes ratings.
   const [isHidden, setHiding] = useState(false);
   const [avgRating, setAvgRating] = useState(Number(recipe.avgRating) || null);
 
-  useEffect(() => {
+  const handleRatingClick = (rating) => {
     const API_URL = "https://grupp3-jynxa.reky.se";
     const POST_RATING_URI = `/recipes/${recipe._id}/ratings`;
     const GET_URI = `/recipes/${recipe._id}`;
@@ -35,7 +34,7 @@ export default function RatingForm({ ratingLevels = [1, 2, 3, 4, 5], confirmatio
         })
         .catch((error) => console.error(error));
     }
-  }, [rating, recipe._id]);
+  };
 
   return (
     <div className="rating-form">
@@ -44,7 +43,7 @@ export default function RatingForm({ ratingLevels = [1, 2, 3, 4, 5], confirmatio
           key={i}
           level={val} // each Star gets an increasing rating value
           confAction={confirmationAction} // thought to popup a "thank for your contribution"
-          setRating={setRating} // user click on Star => set its rating value in the RatingForm
+          setRating={handleRatingClick} // user click on Star => set its rating value in the RatingForm
           recipe={recipe} // optional, put here for logging in the console when user clicks
           isHidden={isHidden} // Star can this way update its css property dynamically
           setHiding={setHiding} // A click on a Star sets hiding for all the Star components (isHidden=true)
