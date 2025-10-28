f202971f19b547828520bf00905e79972f41d02f
Jonathan Boetsch <jonathan.boetsch@gmail.com>
2025-10-28 15:11:58 +0100

    feature(context): rename AvgRatingContext to RecipesContext
    Thought to hold not only average rating but also recipes and isClickable property between the different components of the app.

--- frontend/src/components/AvgRatingContext.jsx
+++
@@ -1,7 +0,0 @@
-import { useContext, createContext } from "react";
-
-export const UpdateAvgRatingContext = createContext(null);
-
-export function useUpdateAvgRating() {
-  return useContext(UpdateAvgRatingContext);
-}
